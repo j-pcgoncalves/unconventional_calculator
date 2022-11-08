a9edf374e49b0da5b8f935dde0a1019b82288844
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-11-08 20:49:20 +0000

    add event listeners to every button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,43 @@
-const defaultResult = 0;
-let currentResult = defaultResult;
+let currentResult = 0;
 
 const getUserNumberInput = () => {
     return parseInt(userInput.value);
 }
 
-const add = () => {
-    const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
-    currentResult += enteredNumber;
+const createAndWriteOutput = (operator, resultBeforeCalc, calcNumber) => {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);
 }
 
+const add = () => {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+const subtract = () => {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+const multiply = () => {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+const divide = () => {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);  
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
